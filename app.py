62a57387c129cadc1e9fc9454492882c5b556152
MichaelVanDingenen <104914142+MichaelVanDingenen@users.noreply.github.com>
2024-07-16 18:20:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uuid
+import base64
 
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+class Item(BaseModel):
+    name:str
+    price:float
+    guid:str
+
+app = FastAPI()
+
+@app.get("/")
+async def read_item():
+    return {"message":"Welcome to the GUID convert app"}
+
+@app.get("/guidconvert/{guid}")
+async def read_item(guid):
+    selfServiceAppointmentID = uuid.UUID(guid)
+    bytearray = selfServiceAppointmentID.bytes_le
+    shortId = base64.urlsafe_b64encode(bytearray).decode('utf-8')
+    shortIdclean = shortId.rsplit("==", 1)[0] + str(shortId.count("="))
+    return {"ConvertedGUID": f"{shortIdclean}"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"message": f"{item.name} is priced at €{item.price}"}
